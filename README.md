1393635b1199fbb94af8479796a3cff15ef27919
mnbf9fca <robert.aleck@cynexia.com>
2019-09-15 23:45:39 +0100

    wall follower complete??

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Final project for Udacity's robotic nanodegree project
  - only works with python 2.7 or you get error `ImportError: dynamic module does not define module export function (PyInit__tf2)` https://github.com/ros/geometry2/issues/411
  - remember to run `rosdep install --from-paths src --ignore-src --rosdistro=kinetic -y`
  - additional dependency `pip install catkin_pkg pyyaml empy rospkg numpy defusedxml`
+ - save using `rosrun map_server map_saver`
--- src/shellScripts/wall_follower.sh
+++ src/shellScripts/wall_follower.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$HOME/catkin_ws/src/world/u.world "&
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch " &
+xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch custom_gmapping_launch_file:=$HOME/catkin_ws/src/turtlebot_simulator/turtlebot_gazebo/launch/gmapping_optimised.launch.xml " &
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
