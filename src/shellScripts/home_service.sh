e8781e07513ff733c7cf26aab6a93ec933feeaaa
Rob Aleck <mnbf9rca@users.noreply.github.com>
2019-09-20 11:57:11 +0100

    source home_service.sh

--- src/shellScripts/home_service.sh
+++ src/shellScripts/home_service.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$PWD/catkin_ws/src/world/irregular-4.world "&
+xterm -e " source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$PWD/catkin_ws/src/world/irregular-4.world "&
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$PWDß/catkin_ws/src/world/irregular-4.yaml" &
+xterm -e " source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$PWDß/catkin_ws/src/world/irregular-4.yaml" &
 sleep 5
-xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+xterm -e " source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
-xterm -e " rosrun add_markers add_markers_node " &
+xterm -e " source devel/setup.bash; rosrun add_markers add_markers_node " &
 sleep 5
-xterm -e " rosrun pick_objects pick_objects_node " 
+xterm -e " source devel/setup.bash; rosrun pick_objects pick_objects_node " 
