a3f9fdb81b551dc1dc15e1a8d380778566d1fa57
mnbf9fca <robert.aleck@cynexia.com>
2019-09-20 11:46:12 +0100

    $HOME - >$PWD

--- src/shellScripts/add_marker.sh
+++ src/shellScripts/add_marker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$HOME/catkin_ws/src/world/irregular-4.world "&
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$PWD/catkin_ws/src/world/irregular-4.world "&
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$HOME/catkin_ws/src/world/irregular-4.yaml" &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$PWD/catkin_ws/src/world/irregular-4.yaml" &
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
--- src/shellScripts/wall_follower.sh
+++ src/shellScripts/wall_follower.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$HOME/catkin_ws/src/world/irregular-4.world "&
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$PWD/catkin_ws/src/world/irregular-4.world "&
 sleep 5
 xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch  " &
 sleep 5
